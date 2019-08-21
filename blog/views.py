b4ac69939dceb7ccc2da8bc980be6005539347ca
f.i.xodjaev <farruhcool1993@gmail.com>
2019-08-21 15:59:04 +0500

    Added template directory with index.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def posts_list(request):
+    n = ['Oleg', 'Vika', 'Masha', 'Lena']
+    return render(request, 'blog/index.html', context={'names': n})
