65a60dc528535d4c8f29bb61b4f96ad59c00e9d5
f.i.xodjaev <farruhcool1993@gmail.com>
2019-08-28 09:29:32 +0500

    Created Tags files

--- blog/urls.py
+++ blog/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
 
     path('', posts_list, name='posts_list_url'),
     path('post/<str:slug>/', post_detail, name='post_detail_url'),
+    path('tags/', tags_list, name='tags_list_url'),
+    path('tag/<str:slug>/', tag_detail, name='tag_detail_url'),
 ]
